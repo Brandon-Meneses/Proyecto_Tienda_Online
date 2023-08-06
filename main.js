54314aa97791ba7d79f8e3fc812a30198f7d2a13
Brandon-Meneses <134113321+Brandon-Meneses@users.noreply.github.com>
2023-08-06 16:44:09 -0500

    Se empezo la logica del carrito de compras

--- main.js
+++ main.js
@@ -1,14 +1,37 @@
-const navMail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
+const menuMail = document.querySelector('.navbar-email');
 const menuHamIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
-navMail.addEventListener('click', toggleDesktopMenu);
+
+menuMail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
 }
 function toggleMobileMenu(){
     mobileMenu.classList.toggle('inactive');
+}
+function toggleCarritoAside(){
+    const ismobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if(!ismobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+    }
+    if(isAsideClosed){
+        //abrir el aside
+       
+    }
+    else{
+        aside.classList.add('inactive');
+    }
+    /*aside.classList.toggle('inactive');*/
+    if(mobileMenu.classList.contains('inactive'));
+
 }
