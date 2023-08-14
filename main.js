4b2462f23dcf83dc2dbfa74ae80f0f4ede4eb4ee
Brandon-Meneses <134113321+Brandon-Meneses@users.noreply.github.com>
2023-08-14 18:26:52 -0500

    Se enlazo la vista de lista de productos y se realizo mediante codigo javascript la maquetacion

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 
 menuMail.addEventListener('click', toggleDesktopMenu);
@@ -11,30 +12,99 @@ menuHamIcon.addEventListener('click', toggleMobileMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 
-function toggleDesktopMenu(){
+function toggleDesktopMenu() {
     const isAsideClosed = aside.classList.toggle('inactive');
-    if(!isAsideClosed)
-    {
+    if (!isAsideClosed) {
         aside.classList.add('inactive');
     }
     mobileMenu.classList.toggle('inactive');
 
     desktopMenu.classList.toggle('inactive');
 }
-function toggleMobileMenu(){
+function toggleMobileMenu() {
     const isAsideClosed = aside.classList.toggle('inactive');
-    if(!isAsideClosed)
-    {
+    if (!isAsideClosed) {
         aside.classList.add('inactive');
     }
     mobileMenu.classList.toggle('inactive');
 }
-function toggleCarritoAside(){
+function toggleCarritoAside() {
     const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
 
-    if(!isMobileMenuClosed){
+    if (!isMobileMenuClosed) {
         mobileMenu.classList.add('inactive');
     }
     aside.classList.toggle('inactive');
 
 }
+const productList = [];
+productList.push({
+    name: 'Bicicleta',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Pantalla',
+    price: 220,
+    image: 'https://images.pexels.com/photos/777001/pexels-photo-777001.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+});
+productList.push({
+    name: 'Computador',
+    price: 620,
+    image: 'https://images.pexels.com/photos/326508/pexels-photo-326508.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+});
+
+/* <div class="product-card">
+        <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+        <div class="product-info">
+          <div>
+            <p>$120,00</p>
+            <p>Bike</p>
+          </div>
+          <figure>
+            <img src="./icons/bt_add_to_cart.svg" alt="">
+          </figure>
+        </div>
+      </div>
+    </div> 
+     */
+
+function renderProducts(arr) {
+    for (product of arr) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+
+        const productImage = document.createElement('img');
+        productImage.setAttribute('src', product.image);
+
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+
+        const productInfoDiv = document.createElement('div');
+
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+        productInfoFigure.appendChild(productImgCart);
+
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+
+        productCard.appendChild(productImage);
+        productCard.appendChild(productInfo);
+
+        cardsContainer.appendChild(productCard);
+
+    }
+}
+renderProducts(productList);
